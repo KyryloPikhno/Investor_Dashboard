22e325372014344f63a5bd10f859dcad8a699040
KyryloPikhno <Mr.Good@i.ua>
2025-05-17 16:39:48 -0600

    Add investment API methods for fetching all investments and by investor ID

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,7 +1,11 @@
+import { PATH } from "@/constants/common"
 import axios from "axios"
 
 const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "",
 })
 
-export default apiClient
+export const investmentsApi = {
+  getAll: () => apiClient.get(PATH.INVESTMENTS),
+  getById: (investorId: string) => apiClient.get(`${PATH.INVESTMENTS}/${investorId}`),
+}
