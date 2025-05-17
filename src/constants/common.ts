fb1bbf53d79cb893ae2e2f2d07d85845b0a098b8
KyryloPikhno <Mr.Good@i.ua>
2025-05-17 17:34:32 -0600

    Add LOGIN path and TABLE_HEADERS constants to common.ts; define PUBLIC_PATHS for public access

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -2,8 +2,20 @@ export const TOKEN_KEY = "token"
 
 export const PATH = {
   INVESTMENTS: "/api/investments",
+  LOGIN: "/login",
 }
 
 export const INVESTOR_ID = "1234-5678"
 
 export const INVESTOR_ID_QUERY_PARAM = "investor-id"
+
+export const TABLE_HEADERS = [
+  "Project",
+  "Token Class",
+  "Shares Owned",
+  "Market Value",
+  "ROI %",
+  "Next Distribution Date",
+]
+
+export const PUBLIC_PATHS = [PATH.LOGIN]
