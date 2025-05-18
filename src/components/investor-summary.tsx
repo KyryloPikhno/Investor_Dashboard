5f56183c6341b4aed6b33abe485f43f36a74ada0
KyryloPikhno <Mr.Good@i.ua>
2025-05-18 01:17:26 -0600

    Refactor InvestorSummary component to improve loading state handling and enhance display of summary details using a mapped structure for better readability.

--- src/components/investor-summary.tsx
+++ src/components/investor-summary.tsx
@@ -1,35 +1,30 @@
+import { Skeleton } from "@/components/skeleton"
 import { InvestorSummaryProps } from "@/types/common"
 import { currencyFormatter } from "@/utils/currency-formatter"
 
-export default function InvestorSummary({ summary }: InvestorSummaryProps) {
+export default function InvestorSummary({ summary, loading }: InvestorSummaryProps) {
+  const items = [
+    { label: "Total Invested", value: summary?.total_invested_amount },
+    { label: "Portfolio Value", value: summary?.portfolio_value },
+    { label: "Distributions Received", value: summary?.distributions_received },
+    { label: "Outstanding Commitments", value: summary?.outstanding_commitments },
+  ]
+
   return (
     <div className="mt-8 mb-4">
       <div className="text-2xl font-semibold">Summary</div>
       <div className="border-t-black border w-full mb-4" />
-      <ul className="list-disc list-inside">
-        <li>
-          Total Invested:{" "}
-          <span className="font-medium">
-            {currencyFormatter.format(summary.total_invested_amount)}
-          </span>
-        </li>
-        <li>
-          Portfolio Value:{" "}
-          <span className="font-medium">{currencyFormatter.format(summary.portfolio_value)}</span>
-        </li>
-        <li>
-          Distributions Received:{" "}
-          <span className="font-medium">
-            {currencyFormatter.format(summary.distributions_received)}
-          </span>
-        </li>
-        <li>
-          Outstanding Commitments:{" "}
-          <span className="font-medium">
-            {currencyFormatter.format(summary.outstanding_commitments)}
-          </span>
-        </li>
-      </ul>
+
+      <div className="space-y-2">
+        {items.map(({ label, value }) => (
+          <div className="flex gap-2 items-center h-6" key={label}>
+            • {label}:
+            <span className="font-medium w-24 h-hull h-6">
+              {loading ? <Skeleton /> : currencyFormatter.format(value)}
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
