1e6192604b14a469493676514c38d3fcc2886d56
KyryloPikhno <Mr.Good@i.ua>
2025-05-19 13:44:36 -0600

    Update build script to include database migration and enhance middleware for token validation, improving security and deployment process.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,19 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
-export function middleware() {
-  // const token = req.cookies.get(TOKEN_KEY)?.value
-  // const pathname = req.nextUrl.pathname
-  // const isPublic = PUBLIC_PATHS.includes(pathname)
+import { PATH, PUBLIC_PATHS, TOKEN_KEY } from "@/constants/common"
 
-  // if (!token && !isPublic) {
-  //   return NextResponse.redirect(new URL(PATH.LOGIN, req.url))
-  // }
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get(TOKEN_KEY)?.value
+  const pathname = req.nextUrl.pathname
+  const isPublic = PUBLIC_PATHS.includes(pathname)
+
+  if (!token && !isPublic) {
+    return NextResponse.redirect(new URL(PATH.LOGIN, req.url))
+  }
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|login|api).*)", "/"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|login).*)", "/"],
 }
