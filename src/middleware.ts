9829481ece1adc093a5d86dd950ba50b167e8ef8
KyryloPikhno <Mr.Good@i.ua>
2025-05-19 12:13:16 -0600

    Remove commented-out code in middleware for cleaner implementation and improved readability.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,13 @@
 import { NextRequest, NextResponse } from "next/server"
 
-import { PATH, PUBLIC_PATHS, TOKEN_KEY } from "@/constants/common"
-
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get(TOKEN_KEY)?.value
-  const pathname = req.nextUrl.pathname
-  const isPublic = PUBLIC_PATHS.includes(pathname)
+  // const token = req.cookies.get(TOKEN_KEY)?.value
+  // const pathname = req.nextUrl.pathname
+  // const isPublic = PUBLIC_PATHS.includes(pathname)
 
-  if (!token && !isPublic) {
-    return NextResponse.redirect(new URL(PATH.LOGIN, req.url))
-  }
+  // if (!token && !isPublic) {
+  //   return NextResponse.redirect(new URL(PATH.LOGIN, req.url))
+  // }
 
   return NextResponse.next()
 }
