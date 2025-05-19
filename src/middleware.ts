afb9cbc5bd463bd05bcfabd4619a0c592772fade
KyryloPikhno <Mr.Good@i.ua>
2025-05-19 11:57:41 -0600

    Refactor middleware to define PUBLIC_PATHS locally and improve path matching logic, enhancing access control for protected routes.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,13 @@
 import { NextRequest, NextResponse } from "next/server"
 
-import { PATH, PUBLIC_PATHS, TOKEN_KEY } from "@/constants/common"
+import { PATH, TOKEN_KEY } from "@/constants/common"
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get(TOKEN_KEY)?.value
   const pathname = req.nextUrl.pathname
-  const isPublic = PUBLIC_PATHS.includes(pathname)
+
+  const PUBLIC_PATHS = ["/login", "/api"]
+  const isPublic = PUBLIC_PATHS.some((path) => pathname.startsWith(path))
 
   if (!token && !isPublic) {
     return NextResponse.redirect(new URL(PATH.LOGIN, req.url))
@@ -15,5 +17,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|login).*)", "/"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)", "/"],
 }
